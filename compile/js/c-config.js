bf9aa4dff46ccabf184a6bd2383b770ed07348bf
vypravime <96717950+vypravime@users.noreply.github.com>
2021-12-28 12:57:25 +0100

    style general layout (bacground-colors, etc.)

--- compile/js/c-config.js
+++ compile/js/c-config.js
@@ -8,3 +8,18 @@ Config.saves.id = "volnosneni";
 Config.saves.version = 1;
 
 Config.passages.nobr = true;
+/*Wrap every processed pasage text into additional div
+  (the div gets either class 'my-passage',
+   or id 'PassageHeader'/'PassageFooter')
+*/
+Config.passages.onProcess = function (p) {
+    let attrsStr = '';
+    if (p.title === 'PassageHeader') {
+        attrsStr = ' id="my-header"';
+    } else if (p.title === 'PassageFooter') {
+        attrsStr = ' id="my-footer"';
+    } else {
+        attrsStr = ' class="my-passage"'
+    }
+	return p.text = `<div${attrsStr}>${p.text}</div>`;
+};
