22e56e8daf3cea15e2522044e5659e47b7f0ea66
vypravime <96717950+vypravime@users.noreply.github.com>
2022-01-01 08:36:46 +0100

    add starting game VolnoSnění via nette presenter

---
+++ app/Presenters/VolnosneniPresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Utils\FileSystem;
+use Nette\Application\Responses\TextResponse;
+
+
+final class VolnosneniPresenter extends Nette\Application\UI\Presenter
+{    
+    public function actionDefault() {
+        $rawHtml = FileSystem::Read(__DIR__ . '/templates/Volnosneni/default.html');
+        $this->sendResponse(new TextResponse($rawHtml));
+    }
+}
