fb63d5fb0307c6e819a2d5470f3b9d7847a45ab5
vypravime <96717950+vypravime@users.noreply.github.com>
2022-01-18 21:35:07 +0100

    adjust directory structure for autodeployment

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -26,7 +26,7 @@ class Bootstrap
 
 		$configurator->addConfig($appDir . '/config/common.neon');
 		$configurator->addConfig($appDir . '/config/services.neon');
-		$configurator->addConfig($appDir . '/config/local.neon');
+		$configurator->addConfig($appDir . '/../shared/config/local.neon');
 
 		return $configurator;
 	}
