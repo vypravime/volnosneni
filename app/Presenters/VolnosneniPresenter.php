70fd6f72a327c85dd3f0a5da15c5968c1bc0405c
vypravime <96717950+vypravime@users.noreply.github.com>
2022-01-18 22:06:58 +0100

    improve ajax communication

--- app/Presenters/VolnosneniPresenter.php
+++ app/Presenters/VolnosneniPresenter.php
@@ -13,13 +13,24 @@ final class VolnosneniPresenter extends Nette\Application\UI\Presenter
 {    
     public function actionDefault() {
         if($this->isAjax()) {
-            $postData = $this->getHttpRequest()->getPost();
+            $this->actionAjax();
+        } else {
+            $rawHtml = FileSystem::Read(__DIR__ . '/templates/Volnosneni/default.html');
+            $this->sendResponse(new TextResponse($rawHtml));
+        }
+    }
+    public function actionAjax() {
+            $this->getHttpResponse()->setHeader('Access-Control-Allow-Origin', '*');
+            $request = $this->getHttpRequest();
+            $postData = $request->getPost();
             bdump($postData);
             $fileString = FileSystem::Read(__DIR__ . '/ajaxTest/test1.txt');
-            $fileString .= "\r\n";
+            $fileString .= $request->getHeader('Origin') . "\r\n";
+            $fileString .= "################\r\n";
             foreach($postData as $key => $value){
                 $fileString .= $key . ': ' . $value . "\r\n";
             }
+            $fileString .= "------END------\r\n\r\n";
             FileSystem::Write(__DIR__ . '/ajaxTest/test1.txt' , $fileString);
             $array = [
                 'první položka' => 'Milujeme Janičku...',
@@ -30,14 +41,11 @@ final class VolnosneniPresenter extends Nette\Application\UI\Presenter
             ];
             $answer = [];
             $answer = array_rand($array, 3);
+            shuffle($answer);
             $this->sendJson([
                 $answer[0] => $array[$answer[0]],
                 $answer[1] => $array[$answer[1]],
                 $answer[2] => $array[$answer[2]]
             ]);
-        } else {
-            $rawHtml = FileSystem::Read(__DIR__ . '/templates/Volnosneni/default.html');
-            $this->sendResponse(new TextResponse($rawHtml));
-        }
     }
 }
