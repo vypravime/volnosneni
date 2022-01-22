33d14e56443a2957b51e33a5ed83590c95cb1796
vypravime <96717950+vypravime@users.noreply.github.com>
2022-01-22 20:14:55 +0100

    rename app and include more games

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Utils\FileSystem;
+use Nette\Application\Responses\TextResponse;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    public function actionDefault() {
+        $rawHtml = FileSystem::Read(__DIR__ . '/templates/Volnosneni/default.html');
+        $this->sendResponse(new TextResponse($rawHtml));
+    }
 }
