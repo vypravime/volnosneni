9603dbf27c1ae2325362aeb5ebdd6920c879b6c2
vypravime <96717950+vypravime@users.noreply.github.com>
2022-01-23 08:12:50 +0100

    add 'noAutoWrapDiv' passage tag + config function

--- compile-twee/js/c-config.js
+++ compile-twee/js/c-config.js
@@ -10,16 +10,21 @@ Config.saves.version = 1;
 Config.passages.nobr = true;
 /*Wrap every processed pasage text into additional div
   (the div gets either class 'my-passage',
-   or id 'PassageHeader'/'PassageFooter')
+   or id 'PassageHeader'/'PassageFooter').
+  If the passage has the tag 'noAutoWrapDiv', no wrapping is done.
 */
 Config.passages.onProcess = function (p) {
-    let attrsStr = '';
-    if (p.title === 'PassageHeader') {
-        attrsStr = ' id="my-header"';
-    } else if (p.title === 'PassageFooter') {
-        attrsStr = ' id="my-footer"';
+    if (p.tags.includes('noAutoWrapDiv')) {
+        return p.text;
     } else {
-        attrsStr = ' class="my-passage"'
+        let attrsStr = '';
+        if (p.title === 'PassageHeader') {
+            attrsStr = ' id="my-header"';
+        } else if (p.title === 'PassageFooter') {
+            attrsStr = ' id="my-footer"';
+        } else {
+            attrsStr = ' class="my-passage"'
+        }
+    	return p.text = `<div${attrsStr}>${p.text}</div>`;
     }
-	return p.text = `<div${attrsStr}>${p.text}</div>`;
 };
