b9fce3a9374dac32689db8256d6ee49dfcd7bfe4
vypravime <96717950+vypravime@users.noreply.github.com>
2022-01-28 13:45:11 +0100

    make app downloadable

---
+++ app/Presenters/DownloadPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\Responses\FileResponse;
+
+
+final class DownloadPresenter extends Nette\Application\UI\Presenter
+{
+    public function actionDefault() {
+        $filePath = __DIR__ . '/templates/Homepage/default.html';
+        $this->sendResponse(new FileResponse($filePath, 'vypravime-0-2-0a.html'));
+    }
+}
