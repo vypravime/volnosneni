e6c65de87a2362f060fc6b6a742c8cd54d66252e
vypravime <96717950+vypravime@users.noreply.github.com>
2022-02-05 16:44:20 +0100

    change dir structure in js-constructors folder

--- compile-twee/js/omega/q-main.js
+++ compile-twee/js/omega/q-main.js
@@ -2,7 +2,7 @@
 
 /**TO DO:
 !! IMPORTANT: SET productionRelease IN DI_CONT CONFIGS TO TRUE !!
-*
+!! pozor na otázky soukromí/bezpečnosti v TěloTaji při zavádění jakéhokoliv AJAXového spojení se serverem !!
 *
 * VS: možnost přidávat nové hráče během hry (viz BeforeStartControlInfo1)
 * v nápovědě dát tlačítko pro uložení hry do souboru (a výzvu to přiložit k emailu)
